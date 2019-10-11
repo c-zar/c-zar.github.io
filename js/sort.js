03a4d9faa7eec38fadcc217fa93e177cec4a1f05
Czar <qaqsd123@gmail.com>
2019-10-10 21:36:28 -0400

    fixed window resizing

--- js/sort.js
+++ js/sort.js
@@ -1,13 +1,13 @@
 var array = [];
 var frame = 0;
-let fr = 30;
+let fr = 60;
 var sorter;
 var canvas;
 var curr, comp;
 
 function setup() {
     var parent = document.getElementById('main');
-    canvas = createCanvas(parent.offsetWidth, 400);
+    canvas = createCanvas(parent.offsetWidth, parent.offsetHeight);
     canvas.parent('canvas');
     fill(255);
     frameRate(fr);
@@ -18,7 +18,7 @@ function init() {
     background(255);
 
     //init array values based on height of screen
-    array.length = height / 10;
+    array.length = (int)(height / 10);
     for (i = 0; i < array.length; i++) {
         array[i] = ((i + 1) / array.length) * height;
     }
@@ -41,7 +41,7 @@ function draw() {
     fill('#130b15');
     for (let l = 0; l < array.length; l++) {
 
-        //change color based on
+        //change keeps track of current index and index we are comparing to
         if (l == curr) {
             fill('#2aa84c');
         } else if (l == comp) {
@@ -52,7 +52,7 @@ function draw() {
 
         //draw rectangle (xpos, ypos, width, height);
         //canvas 0,0 = top left
-        rect(l * (width / array.length), height+4, (width / array.length), -array[l]);
+        rect(l * (width / array.length), height + 4, (width / array.length), -array[l]);
 
     }
     if (sorter.next().done) {
@@ -65,7 +65,7 @@ function* bubblesort() {
         for (let z = 0; z < array.length - x - 1; z++) {
             curr = z;
             comp = z + 1;
-            yield;
+            yield;                              //yield to so we can redraw array
             if (array[z] > array[z + 1]) {
                 //Swap the numbers
                 let tmp = array[z]; //Temporary variable to hold the current number
